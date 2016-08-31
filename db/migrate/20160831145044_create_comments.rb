8fceae560723239f9d254759ea216a58c4322655
Nicholas Davidson <ndavidson.sf@me.com>
2016-08-31 16:21:22 -0700

    Fixes syntax error on line 14 in comments migration

--- db/migrate/20160831145044_create_comments.rb
+++ db/migrate/20160831145044_create_comments.rb
@@ -11,6 +11,6 @@ class CreateComments < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :comments [:commentable_type, :commentable_id]
+    add_index :comments, [:commentable_type, :commentable_id]
   end
 end
