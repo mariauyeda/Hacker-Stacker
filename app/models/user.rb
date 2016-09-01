e036d94cbb5acd922f977c0ba51f9670ccc12c27
Nicholas Davidson <ndavidson.sf@me.com>
2016-09-01 10:14:06 -0700

    Adds BCrypt logic to USER model/Deletes model README

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
+
